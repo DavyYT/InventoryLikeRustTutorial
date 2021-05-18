a263d12f67a20b627894f035567c8f67f730df63
DavyYT <84409326+DavyYT@users.noreply.github.com>
2021-05-18 16:28:03 -0700

    Add files via upload

---
+++ CraftingSlot.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftingSlot : MonoBehaviour
+{
+    public RequiredItem[] itemsNeeded;
+
+    public Inventory inv;
+
+    public GameObject craftedItem;
+    public int craftedItemAmount;
+
+    public void CraftItem()
+    {
+        foreach(RequiredItem i in itemsNeeded)
+        {
+            if (inv.GetItemAmount(i.itemID) < i.amountNeeded)
+                return;
+        }
+
+        foreach(RequiredItem i in itemsNeeded)
+        {
+            inv.RemoveItemAmount(i.itemID, i.amountNeeded);
+        }
+
+        Item z = Instantiate(craftedItem, Vector3.zero, Quaternion.identity).GetComponent<Item>();
+        z.amountInStack = craftedItemAmount;
+        inv.AddItem(z);
+    }
+}
+
+[System.Serializable]
+public class RequiredItem
+{
+    public int itemID;
+    public int amountNeeded;
+}
---
+++ CustomJSON.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomJSON : MonoBehaviour
+{
+    public static List<T> FromJson<T>(string json)
+    {
+        Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(json);
+        return wrapper.items;
+    }
+
+    public static string ToJson<T>(List<T> list)
+    {
+        Wrapper<T> wrapper = new Wrapper<T>();
+        wrapper.items = list;
+        return JsonUtility.ToJson(wrapper);
+    }
+
+    [System.Serializable]
+    public class Wrapper<T>
+    {
+        public List<T> items;
+    }
+}
